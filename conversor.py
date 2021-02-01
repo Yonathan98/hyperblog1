ea611291c0e91eaf204ca136d123405fe15a39bd
Yonathan Layton <yonathan98@misena.edu.co>
2021-02-01 12:44:41 -0500

    Agregando funciones

--- conversor.py
+++ conversor.py
@@ -1,3 +1,10 @@
+def conversor (tipo_pesos, valor_dolar):
+    pesos = input ("¿Cuantos pesos" + tipo_pesos + "tiene?: ")
+    pesos = float(pesos)
+    dolares = pesos/ valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes $" + dolares + " dolares")
 menu= """
 Bienvenido al conversor de monedas ❤
 
@@ -9,29 +16,11 @@ Elige una opcion
 """
 opcion= input(menu)
 if opcion == '1':
-    pesos = input ("¿Cuantos pesos colombianos tiene?: ")
-    pesos = float(pesos)
-    valor_dolar = 3875
-    dolares = pesos/ valor_dolar
-    dolares = round(dolares, 2)
-    dolares = str(dolares)
-    print("Tienes $" + dolares + " dolares")
+    conversor("colombianos", 3875)
 elif opcion == '2':
-    pesos = input ("¿Cuantos pesos argentinos tiene?: ")
-    pesos = float(pesos)
-    valor_dolar = 65
-    dolares = pesos/ valor_dolar
-    dolares = round(dolares, 2)
-    dolares = str(dolares)
-    print("Tienes $" + dolares + " dolares")
+    conversor("argentinos", 65)
 elif opcion == '3':
-    pesos = input ("¿Cuantos pesos mexicanos tiene?: ")
-    pesos = float(pesos)
-    valor_dolar = 24
-    dolares = pesos/ valor_dolar
-    dolares = round(dolares, 2)
-    dolares = str(dolares)
-    print("Tienes $" + dolares + " dolares")
+    conversor("mexicaanos", 24)
 else:
     print ("No ingreso ninguna moneda")   
 
