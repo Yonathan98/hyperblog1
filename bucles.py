fe5a0692b5c1a6c571a6021c82038f411ff6d002
Yonathan Layton <yonathan98@misena.edu.co>
2021-02-02 12:44:47 -0500

    Otras clases de programacion

--- bucles.py
+++ bucles.py
@@ -1,11 +1,31 @@
-def run(num, rept):
-    if num <= rept:
-        cont = num
-        print(str(2 ** cont) )
-        run(num+1, rept)
-    else:
-        print("Fin!")
+#def run(num, rept):
+ #   if num <= rept:
+  #      cont = num
+   #     print(str(2 ** cont) )
+    #    run(num+1, rept)
+    #else:
+     #   print("Fin!")
 
-if __name__ == "__main__":
-    repeticiones = int(input("Cuantas potencias: "))
-    run(0, repeticiones)
+#if __name__ == "__main__":
+   # repeticiones = int(input("Cuantas potencias: "))
+    #run(0, repeticiones)
+def run():
+    #limite = 1000
+    #contador = 0
+    #potencia_2= 2**contador
+    #while potencia_2 < limite:
+       # print("2 elevedo a " + str(contador) + "es igual a" + str(potencia_2)) 
+        #contador = contador + 1
+        #potencia_2= 2**contador
+    limite=100 
+    contador=0
+    while contador < limite:
+        print(contador)
+        contador= contador+1
+        if contador == 20:
+            continue #aqui se pone break o continue
+
+
+
+if __name__ == '__main__':
+    run()
